a842a2cec7395e597ec8295d55089d59a36ea98e
AxelCornand <axelcornand@msn.com>
2022-12-19 11:41:35 +0100

    rendu dynamique

---
+++ php/inc.php
@@ -0,0 +1,26 @@
+<?php
+     $gitLinks = "https://github.com/AxelCornand";
+     $contact = [
+        "0685263219",
+        "axelcornand@msn.com",
+        "https://www.linkedin.com/in/axel-cornand-592106103/"
+     ];
+     $softSkills = [
+        "Prise de décisions.",
+        "Esprit critique.",
+        "Bon communiquant.",
+        "collabore facilement avec les autres.",
+        "Créatif",
+        "Apprend continuellement de nouvelles choses.",
+        "Organisé",
+        "Intelligence émotionnelle."
+     ];
+     $skill = [
+        "HTML / Twig",
+        "CSS / Bootstrap",
+        "PHP / Symfony",
+        "SQL / Doctrine"
+     ];
+
+?>
+
---
+++ php/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Curiculum vitae</title>
+</head>
+<body>
+    <?php require './inc.php' ?>
+    <header>
+
+    </header>
+    <main>
+        <section class="skills">
+            <h1><strong>Axel Cornand</strong></h1>
+            <h2><strong>Développeur Web "back-end"!</strong></h2>
+            <article class="softSkills">
+                <h3><strong>Soft-Skills</strong></h3>
+                <ul>
+                   <?php for ( $indexSoftSkill = 0 ; $indexSoftSkill < count( $softSkills ) ; $indexSoftSkill++ ){ ?>
+                    <li>
+                    <?php 
+                        echo $softSkills[ $indexSoftSkill ]; 
+                    ?>
+                    </li>
+                    <?php }  ?>
+                </ul>
+            </article>
+            <article class="skill">
+            <h3><strong>Skill</strong></h3>
+                <ul>
+                   <?php for ( $indexSkill = 0 ; $indexSkill < count( $skill ) ; $indexSkill++ ){ ?>
+                    <li>
+                    <?php 
+                        echo $skill[ $indexSkill ]; 
+                    ?>
+                    </li>
+                    <?php }  ?>
+                </ul>
+            </article>
+            <article class="projet">
+                <h3><strong>Projets</strong></h3>
+                <a href=<?php echo $gitLinks ?> target="blank">AxelCornand-Github</a>
+
+            </article>
+            <article class="contact">
+                <h3><strong>Contact!</strong></h3>
+                    <p><?php echo $contact[0] ?></p>
+                    <p><?php echo $contact[1] ?></p>
+                    <a href=<?php echo $contact [2] ?> target="blank">@linkedin</a>     
+            </article>
+        </section>
+    </main>
+</body>
+</html>
