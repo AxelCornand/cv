ea202a13e613138a4b88d006c368898180107aa6
AxelCornand <axelcornand@msn.com>
2023-02-04 11:06:58 +0100

    test

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Curiculum vitae</title>
 </head>
 <body>
-    <?php require './inc.php' ?>
+    <?php require __DIR__.'/php/inc.php' ?>
     <header>
 
     </header>
